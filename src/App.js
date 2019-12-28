d7ce1920e7f0e34e373c9bf9ad3b7a3321ab51e1
Alexander Hoinville <ahoin001@gmail.com>
2019-12-28 12:03:45 -0500

    Render list with provided props

--- src/App.js
+++ src/App.js
@@ -5,12 +5,18 @@ import './App.css'
 
 const App = () => {
 
+  const goalItems = [
+    {id: 'g1', task: 'Understand React Workflow'},
+    {id: 'g2', task: 'Understand Express Node workflow'},
+    {id: 'g3', task: 'Be able to create MERN App'}
+  ]
+
   return (
     <div className="course-goals">
 
       <h2>Course Goals</h2>
 
-      <GoalList />
+      <GoalList List={goalItems} />
 
     </div>
 
