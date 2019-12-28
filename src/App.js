3a4c0360e928fe574a851e4673ddb00124e3a5ca
Alexander Hoinville <ahoin001@gmail.com>
2019-12-28 15:22:13 -0500

    Use usestate callback component to rerender components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+// useState to have access to hooks 
+//  They let you use state and other React features without writing a class.
+import React, { useState } from 'react';
 import GoalList from './components/GoalList/GoalList'
 import { NewGoal } from "./components/NewGoal/NewGoal";
 
@@ -6,17 +8,29 @@ import './App.css'
 
 const App = () => {
 
-  // Start Data
-  const goalItems = [
+  // first constant holds latest state snapshot
+  // second constant is function to manipulate state
+  const [courseGoals, setCourseGoals] = useState([
     { id: 'g1', task: 'Understand React Workflow' },
     { id: 'g2', task: 'Understand Express Node workflow' },
     { id: 'g3', task: 'Be able to create MERN App' }
-  ]
+  ])
 
-  // Recieves goal object and passes it to list data
-  const addNewGoalHandler = (newGoal) => {
-    goalItems.push(newGoal)
-    console.log(goalItems);
+  // Recieves goal object, then creates new array to replace state
+  const addNewGoalHandler = newGoal => {
+
+    // SetCourse Goals wants a new array/object/etc to be returned and replace current one
+    // concat will return new array, since our state is an array
+    // could not use push because push modifies exsisting array
+    // setCourseGoals(courseGoals.concat(newGoal))
+    
+
+    // Bulletproof way to do it and have all updates processed in order
+    // Uses callback functions that recieves latest state, then provides new state
+    setCourseGoals((prevCourseGoal) => {
+      return prevCourseGoal.concat(newGoal)
+    })
+
   }
 
 
@@ -28,7 +42,7 @@ const App = () => {
 
       {/* onAddGoal is naming convention but could be anything (Write nice code alex) */}
       <NewGoal onAddGoal={addNewGoalHandler} />
-      <GoalList List={goalItems} />
+      <GoalList List={courseGoals} />
 
     </div>
 
