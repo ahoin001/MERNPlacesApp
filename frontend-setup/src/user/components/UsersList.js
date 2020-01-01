3cf09e902ce1c457b8df5ce0e3404e24ce4b1ab2
Alexander Hoinville <ahoin001@gmail.com>
2019-12-31 22:25:49 -0500

    Add avatar component to manipulate images, and links to all user items that lead to unique pages

---
+++ frontend-setup/src/user/components/UsersList.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+import UserItem from './UsersItem'
+import './UsersList.css'
+
+/*
+    Component that is a list that displays users
+*/
+
+export default function UsersList(props) {
+
+    // If the user list is empty, then notify front end with div
+    if (props.items.length === 0) {
+        return (
+            <div className="center">
+                <h2>No users found.</h2>
+            </div>
+        )
+    }
+
+    // Else If there is a list, retur a user item for each user on the list given user info
+    return (
+        <ul className='users-list'>
+            {props.items.map(user => {
+                return (
+
+                    <UserItem
+                        key={user.id}
+                        id={user.id}
+                        image={user.image}
+                        name={user.name}
+                        placeCount={user.placeCount}
+                    />
+
+                )
+            })}
+        </ul>
+    )
+}
