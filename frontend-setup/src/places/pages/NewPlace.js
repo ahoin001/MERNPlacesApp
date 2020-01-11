a33ef6bc55c1849f32912d987131854631a55ab2
Alexander Hoinville <ahoin001@gmail.com>
2020-01-10 19:06:55 -0500

    Create New Place page, which uses a Input component that returns form elements depending on provided props

--- frontend-setup/src/places/pages/NewPlace.js
+++ frontend-setup/src/places/pages/NewPlace.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
+import Input from '../../shared/components/FormElements/Input'
+
+import './NewPlace.css'
+
+/*
+    Page component where user can fill out form and add a nw place
+*/
 export const NewPlace = () => {
+
     return (
-        <h2>
-           New Place! 
-        </h2>
+        <form className="place-form">
+
+            <Input
+                element='input'
+                type='text'
+                label='title'
+            />
+
+        </form>
     )
+
 }
