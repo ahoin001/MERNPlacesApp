d213a5dcad3aad9fd77d8ae95c1e120f58b55a39
Alexander Hoinville <ahoin001@gmail.com>
2020-01-17 23:03:45 -0500

    Create custom hook that can be used with our forms and inputs

--- frontend-setup/src/places/pages/NewPlace.js
+++ frontend-setup/src/places/pages/NewPlace.js
@@ -1,110 +1,36 @@
-import React, { useCallback, useReducer } from 'react'
+import React from 'react'
 
+import useForm from '../../shared/components/hooks/form-hook'
 import Input from '../../shared/components/FormElements/Input'
-import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from "../../shared/components/Util/Validator";
 import Button from '../../shared/components/FormElements/Button';
+import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from "../../shared/components/Util/Validator";
 
 import './PlaceForm.css'
 
-const formReducer = (state, action) => {
-
-    switch (action.type) {
-
-        case 'INPUT_CHANGE':
-            let formIsValid = true;
-            // console.log(formIsValid)
-
-            // input id will be the name of properties in state.inputs
-            for (let inputId in state.inputs) {
-
-                // checks if the name of the property is same as input from action 
-                if (inputId === action.inputId) {
-
-                    // console.log(`IN IF- THE INPUTID: ${inputId} and actionId: ${action.inputId}
-                    // AND IS FORM VALID ${action.isValid}`)
-
-                    // form is valid is true if formIsValid and action.isValid are true
-                    formIsValid = formIsValid && action.isValid;
-
-                    // console.log(formIsValid)
-
-                }
-
-                else {
-                    // console.log(`IN ELSE- THE INPUTID: ${inputId} and actionId: ${action.inputId}
-                    // AND IS FORM VALID ${action.isValid}`)
-
-                    formIsValid = formIsValid && state.inputs[inputId].isValid;
-
-                    // console.log(`FROM ELSE AFTER CECKING OOTHER INNPUT${formIsValid}`)
-                }
-            }
-
-            return {
-
-                ...state,
-
-                // and then over ride necessary info
-                inputs: {
-
-                    // Copies Properties of inputs object
-                    ...state.inputs,
-
-                    // Over ride the inputs property (title or description) that sent action
-                    // Will add new property if input is not already in state (ex/ Adress is not in original state)
-                    [action.inputId]: { value: action.value, isValid: action.isValid }
-                },
-
-                // YOUR PROBLEM IS THIS NOT BEING TRUE WHEN IT IS SUPPOSED TO BE
-                isValid: formIsValid
-
-            }
-
-        default:
-            return state;
-
-    }
-}
-
 /*
-    Page component where user can fill out form and add a nw place
+    Page component where user can fill out form and add a new place
 */
 export const NewPlace = () => {
 
-    // inputs keeps track of validity of multiple inputs, isValid for if overall form is valid
-    const [formState, dispatch] = useReducer(formReducer, {
-        inputs: {
-            title: {
-                value: '',
-                isValid: false
-            },
-            description: {
-                value: '',
-                isValid: false
-            },
-            adress: {
-                value: '',
-                isValid: false
-            }
+    // Custom Hook needs object of inputs, and initial form validity
+    const [formState, inputHandler] = useForm({
 
+        title: {
+            value: '',
+            isValid: false
         },
-        isValid: false
-    })
+        description: {
+            value: '',
+            isValid: false
+        },
+        adress: {
+            value: '',
+            isValid: false
+        }
 
-    // Recieves id to find, value to replace, and isValid to make sure its okay
-    // useCallback make sure component reuses this function on rerender instead of causing infinite loop with useEffect in 
-    const inputHandler = useCallback((id, value, isValid) => {
-
-        // Passes action object to reducer function
-        dispatch({
-            type: 'INPUT_CHANGE',
-            value: value,
-            isValid: isValid,
-            inputId: id
-        })
-
-    }, [])
-
+    },
+        false
+    )
 
     const placeSubmitHandler = event => {
         // Prevent form from refreshing page which would ruin react render
